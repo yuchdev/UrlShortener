e3bd10fb30cd57844d845ca84cc039a56997ef1b
Yurii Cherkasov <strategarius@protonmail.com>
2026-05-13 15:18:55 +0700

    Fix hiredis discovery fallback and expand redis unit behavior tests

--- test/unit/storage/redis/06_clear_by_prefix_strategy.cpp
+++ test/unit/storage/redis/06_clear_by_prefix_strategy.cpp
@@ -1,8 +1,8 @@
-#define BOOST_TEST_MODULE 06_clear_by_prefix_strategy
+#define BOOST_TEST_MODULE RedisClearByPrefixStrategy
 #include <boost/test/unit_test.hpp>
-#include "url_shortener/storage/redis/RedisCacheSerialization.hpp"
-#include "url_shortener/storage/redis/RedisCacheStore.hpp"
 
-BOOST_AUTO_TEST_CASE(smoke_06_clear_by_prefix_strategy) {
-    BOOST_TEST(true);
+#include "url_shortener/storage/redis/RedisCacheSerialization.hpp"
+
+BOOST_AUTO_TEST_CASE(prefix_isolated_keyspace) {
+    BOOST_TEST(redis_cache::BuildCacheKey("svc-a:link:", "x") != redis_cache::BuildCacheKey("svc-b:link:", "x"));
 }
