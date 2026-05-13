6bd3c29a3f1e464445330f04d89c693839257e2e
Yurii Cherkasov <strategarius@protonmail.com>
2026-05-13 21:29:45 +0700

    Fix hiredis include compatibility for fetched and system headers

--- src/storage/redis/RedisCacheStore.cpp
+++ src/storage/redis/RedisCacheStore.cpp
@@ -1,6 +1,12 @@
 #include "url_shortener/storage/redis/RedisCacheStore.hpp"
 
+#if __has_include(<hiredis/hiredis.h>)
 #include <hiredis/hiredis.h>
+#elif __has_include(<hiredis.h>)
+#include <hiredis.h>
+#else
+#error "hiredis headers not found"
+#endif
 
 #include "url_shortener/storage/redis/RedisCacheSerialization.hpp"
 
