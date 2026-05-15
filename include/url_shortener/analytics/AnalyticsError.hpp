d5e1d0d19c7284173749280b2ff85241c8c89acb
Yurii Cherkasov <strategarius@protonmail.com>
2026-05-16 00:59:44 +0700

    Fix analytics error streaming for Boost.Test diagnostics

--- include/url_shortener/analytics/AnalyticsError.hpp
+++ include/url_shortener/analytics/AnalyticsError.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ostream>
 #include <string>
 
 namespace url_shortener::analytics {
@@ -23,5 +24,29 @@ struct AnalyticsError {
     AnalyticsErrorCode code = AnalyticsErrorCode::none;
     std::string message;
 };
+
+/**
+ * @brief Stream formatter for analytics error codes.
+ */
+inline std::ostream& operator<<(std::ostream& os, AnalyticsErrorCode code)
+{
+    switch (code) {
+    case AnalyticsErrorCode::none:       return os << "AnalyticsErrorCode::none";
+    case AnalyticsErrorCode::validation: return os << "AnalyticsErrorCode::validation";
+    case AnalyticsErrorCode::queue:      return os << "AnalyticsErrorCode::queue";
+    case AnalyticsErrorCode::repository: return os << "AnalyticsErrorCode::repository";
+    case AnalyticsErrorCode::worker:     return os << "AnalyticsErrorCode::worker";
+    case AnalyticsErrorCode::retention:  return os << "AnalyticsErrorCode::retention";
+    }
+    return os << "AnalyticsErrorCode(" << static_cast<int>(code) << ")";
+}
+
+/**
+ * @brief Stream formatter for analytics error values.
+ */
+inline std::ostream& operator<<(std::ostream& os, const AnalyticsError& error)
+{
+    return os << "AnalyticsError{code=" << error.code << ", message='" << error.message << "'}";
+}
 
 } // namespace url_shortener::analytics
